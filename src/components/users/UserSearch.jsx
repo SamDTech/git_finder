2f8cd3c509bd1eff74b2d8eac2bd5b5d43491a6e
sam <opeyemisamuel222@gmail.com>
2022-02-07 22:34:41 +0100

    done

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,30 +1,30 @@
-import React, { useState, useContext } from "react";
+import { useState, useContext } from "react";
+
 import AlertContext from "../../context/alert/AlertContext";
+import { searchUsers } from "../../context/github/GithubActions";
 import GithubContext from "../../context/github/GithubContext";
 
-const UserSearch = () => {
+function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users, searchUsers, clearUsers } = useContext(GithubContext);
-
+  const { users, dispatch } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext);
 
-  const handleChange = (e) => {
-    setText(e.target.value);
-  };
+  const handleChange = (e) => setText(e.target.value);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (text === "") {
       setAlert("Please enter something", "error");
     } else {
-
-      searchUsers(text);
+      dispatch({ type: "SET_LOADING" });
+      const users = await searchUsers(text);
+      dispatch({ type: "GET_USERS", payload: users });
 
       setText("");
     }
   };
-
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
@@ -34,14 +34,14 @@ const UserSearch = () => {
             <div className="relative">
               <input
                 type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                placeholder="Search"
                 value={text}
                 onChange={handleChange}
-                className="w-full pr-40 bg-gray-200 input input-lg text-black"
-                placeholder="Search ..."
               />
               <button
-                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
                 type="submit"
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
               >
                 Go
               </button>
@@ -49,9 +49,18 @@ const UserSearch = () => {
           </div>
         </form>
       </div>
-      {users.length > 0 && <div className="btn btn-ghost btn-lg" onClick={clearUsers}>Clear</div>}
+      {users.length > 0 && (
+        <div>
+          <button
+            onClick={() => dispatch({ type: "CLEAR_USERS" })}
+            className="btn btn-ghost btn-lg"
+          >
+            Clear
+          </button>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default UserSearch;
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,6 +6,13 @@ const githubReducer = (state, action) => {
       return { ...state, users: action.payload, loading: false };
     case "GET_USER":
       return { ...state, user: action.payload, loading: false };
+    case "GET_USER_AND_REPOS":
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        loading: false,
+      };
     case "CLEAR_USERS":
       return { ...state, users: [], loading: false };
     default:
