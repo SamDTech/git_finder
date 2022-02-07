69f5a32e2ea3656fe49e31671c9a7e7a16b4dbe6
sam <opeyemisamuel222@gmail.com>
2022-02-07 19:19:51 +0100

    setup alert reducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import { AlertProvider } from "./context/alert/AlertContext";
 import { GithubProvider } from "./context/github/GithubContext";
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -9,22 +10,24 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/about" exact element={<About />} />
-              <Route path="/notfound" exact element={<NotFound />} />
-              <Route path="/*" exact element={<NotFound />} />
-            </Routes>
-          </main>
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/about" exact element={<About />} />
+                <Route path="/notfound" exact element={<NotFound />} />
+                <Route path="/*" exact element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
