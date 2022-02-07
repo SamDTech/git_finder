3d2f3979be235aaba959e7c5b269f4ffd172e7fa
sam <opeyemisamuel222@gmail.com>
2022-02-07 16:38:39 +0100

    adding global state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import { GithubProvider } from "./context/github/GithubContext";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/about" exact element={<About />} />
-            <Route path="/notfound" exact element={<NotFound />} />
-            <Route path="/*" exact element={<NotFound />} />
-          </Routes>
-        </main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/about" exact element={<About />} />
+              <Route path="/notfound" exact element={<NotFound />} />
+              <Route path="/*" exact element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
